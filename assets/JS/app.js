eef5cdc661387a7941d3cee110bb1553140d691b
Luis Pena <jansgreen@gmail.com>
2019-08-30 13:03:37 -0400

    Modificated from Branch_Menu_Upset

---
+++ assets/JS/app.js
@@ -0,0 +1,37 @@
+
+
+function I_vermas(id){
+    if(id=="mas"){
+    document.getElementById("desplegar").style.display="block";   
+    document.getElementById("mas").style.display="none"; 
+    }
+    else{
+    document.getElementById("desplegar").style.display="none";
+    document.getElementById("mas").style.display="inline";
+    }
+}
+
+function II_vermas(id){
+    if(id=="II_mas"){
+    document.getElementById("II_desplegar").style.display="block";   
+    document.getElementById("II_mas").style.display="none"; 
+    }
+    else{
+    document.getElementById("II_desplegar").style.display="none";
+    document.getElementById("II_mas").style.display="inline";
+    }
+}
+
+function III_vermas(id){
+    if(id=="III_mas"){
+    document.getElementById("III_desplegar").style.display="block";   
+    document.getElementById("III_mas").style.display="none"; 
+    }
+    else{
+    document.getElementById("III_desplegar").style.display="none";
+    document.getElementById("III_mas").style.display="inline";
+    }
+}
+    
+
+
