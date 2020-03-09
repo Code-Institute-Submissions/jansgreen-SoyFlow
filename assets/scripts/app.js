092cf6b266506a791cc4399174bd2a9085cfd8f6
Luis Pena <Luis Pena>
2020-03-09 18:57:59 -0400

    I test the Auht of google fixing 15

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -152,66 +152,88 @@ function singSendding() {
 API OF YOUTUBE
 ===============================================================*/
 
-function authenticate() {
-  return gapi.auth2
-    .getAuthInstance()
-    .signIn({
-      scope:
-        "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtubepartner-channel-audit"
-    })
-    .then(
-      function() {
-        console.log("Sign-in successful");
-      },
-      function(err) {
-        console.error("Error signing in", err);
-      }
-    );
-}
-function loadClient() {
-  gapi.client.setApiKey("AIzaSyBs-BPSMqxLUwSi9UJ27ltcNMRTxMEMOyg");
-  return gapi.client
-    .load("https://content.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-    .then(
-      function() {
-        console.log("GAPI client loaded for API");
-      },
-      function(err) {
-        console.error("Error loading GAPI client for API", err);
-      }
-    );
-}
+const CLIENT_ID = '522228945921-6q3pk6hsaajtphi8pj466k4sgchds5c9.apps.googleusercontent.com';
+const DISCOVERY_DOCS = [
+  'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
+];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
-function requestUserUploadsPlaylistId() {
-  // See https://developers.google.com/youtube/v3/docs/channels/list
-  var request = gapi.client.youtube.channels.list({
-    mine: true,
-    part: 'contentDetails'
-  });
-  request.execute(function(response) {
-    playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
-    requestVideoPlaylist(playlistId);
-  });
-}
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
 
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function execute() {
-  return gapi.client.youtube.channels.list({}).then(
-    function(response) {
-      // Handle the results here (response.result has the parsed body).
-      console.log("Response", response);
-    },
-    function(err) {
-      console.error("Execute error", err);
-    }
-  );
-}
-gapi.load("client:auth2", function() {
-  gapi.auth2.init({ client_id:  "522228945921-6q3pk6hsaajtphi8pj466k4sgchds5c9.apps.googleusercontent.com"
-  });
+const defaultChannel = 'PLcUqPeI0P9OzpYK1UBbeAApBHkEXONi5K';
 
+// Form submit and change channel
+channelForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const channel = channelInput.value;
+
+  getChannel(channel);
 });
 
+// Load auth2 library
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+// Init API client library and set up sign in listeners
+function initClient() {
+  gapi.client
+    .init({
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES
+    })
+    .then(() => {
+      // Listen for sign in state changes
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      // Handle initial sign in state
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+// Update UI sign in state changes
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    content.style.display = 'block';
+    videoContainer.style.display = 'block';
+    gapi.client.name = 'block';
+    getChannel(defaultChannel);
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+    content.style.display = 'none';
+    videoContainer.style.display = 'none';
+    gapi.client.name = 'none';
+  }
+}
+
+// Handle login
+function handleAuthClick() {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+// Handle logout
+function handleSignoutClick() {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+// Display channel data
+function showChannelData(data) {
+  const channelData = document.getElementById('channel-data');
+  channelData.innerHTML = data;
+}
+
+// Get channel from API
 function getChannel(channel) {
   gapi.client.youtube.channels
     .list({
@@ -250,420 +272,41 @@ function getChannel(channel) {
     .catch(err => alert('No Channel By That Name'));
 }
 
-
-/*=====================================
-GOOGLE TOKEN
-=======================================
-https://accounts.google.com/o/oauth2/auth?
-  client_id=522228945921-6q3pk6hsaajtphi8pj466k4sgchds5c9.apps.googleusercontent.com&
-  redirect_uri=https://jansgreen.github.io/SoyFlow/daskboard.html&
-  scope=https://www.googleapis.com/auth/youtube&
-  response_type=token
-
-  curl -H "Authorization: Bearer ACCESS_TOKEN" https://www.googleapis.com/youtube/v3/channels?part=id&mine=true
-
-/*========================
-prueba
-================*/
-
-var contenido = document.querySelector('#data')
-function spotifyButton() {
-var user_id = "lyo60r7xrt5cexv4nm7uw4wd2";
-var token = "Bearer BQAX4QnLuSdR9D1WVtKSrYBcbixBmNY5lB4619AXCkmiH5YHa1iCAFTOJjFcRgAPxtyINsVQsZLbt5DaQYrzYKOJwwfnA1bDH46Cn8RVBjOkM6HYzbZfSUAOB3C6B3DWJWoldhnFkhmbmsVvTrscC56aNvqXiEuD6w";
-var playlist_url="https://api.spotify.com/v1/playlists/"+user_id+"/playlists";
-    fetch('https://accounts.spotify.com/authorize?client_id=8402bf679f8b4e6c975686305c2bc15f&redirect_uri=https%3A%2F%2Fjansgreen.github.io%2FSoyFlow%2Fdaskboard&response_type=code')
-    .then($.ajax({
-      url: playlist_url, headers:{Authorization:token},
-      method: "GET",
-      mode: 'no-cors',
-      success:function(response){
-        console.log(response); }
-    })
-    )
-    .then(res => res.json())
-    .then(data => { data
-        console.log(response)
-
-    })
+// Add commas to number
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+function requestVideoPlaylist(playlistId) {
+  const requestOptions = {
+    playlistId: playlistId,
+    part: 'snippet',
+    maxResults: 10
+  };
 
-/*=====================*//*
-/* After the API loads, call a function to enable the search box.
-function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
-}
+  const request = gapi.client.youtube.playlistItems.list(requestOptions);
 
-// Search for a specified string.
-function search() {
-  var q = $('#query').val();
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet'
-  });
+  request.execute(response => {
+    console.log(response);
+    const playListItems = response.result.items;
+    if (playListItems) {
+      let output = '<br><h4 class="center-align">Latest Videos</h4>';
 
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#search-container').html('<pre>' + str + '</pre>');
-  });
-}*/
-/*
-function traer() {
-var user_id = "lyo60r7xrt5cexv4nm7uw4wd2";
-const url = "https://api.spotify.com/v1/playlists/"+user_id+"/playlists";
+      // Loop through videos and append output
+      playListItems.forEach(item => {
+        const videoId = item.snippet.resourceId.videoId;
 
-const options = {
-  headers: {
-    Authorization: "Bearer BQDA8ph-zV8MH_dvqD0WtbseTdZgBNAXsYvLW7AgKA03vhw3wsTmI8G3NMwdz8PLdTsOISW8Vn6-xkBb477ITedPOgC1sCo2WYHJK9uDNnlQnx9akptIt3oX8RHtnaiNVv5NL8aGymG9cPKgeG6ohR507lZnqU1kNFku_wtPENIMqcFBeguKfhxO-R2F2QWkrAspjQ-6_A3t2wCh-BnlR0ESNULwz6C2wEBtEu4I0tBUDqGf4QB_Bc1t9zCzXRsWrsaMRYwrkLDsplfqYltDKrRdfx4IDw"
-  }
-};
-
-fetch(url, options)
-  .then( res => res.json())
-  .then( data => console.log(data) );
-}*/
-
-/*
-$(document).ready(function() {
-  $.get(
-    "www.googleapis.com/youtube/v3/channels", {
-      part: 'contentDetails',
-      id: 'Wj_WdivxjMg',
-      key: 'AIzaSyBs-BPSMqxLUwSi9UJ27ltcNMRTxMEMOyg' },
-    function(data) {
-      $.each(data.items, function(i, item) {
-        pid=item.contentDetails.relatedPlaylistis.uploads;
-        fetchVideo(paid);
-        console.log(item);
+        output += `
+          <div class="col s3">
+          <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        `;
       });
+
+      // Output videos
+      videoContainer.innerHTML = output;
+    } else {
+      videoContainer.innerHTML = 'No Uploaded Videos';
     }
-  );
-function fetchVideo(pid) {
-  $.get(
-    "https://www.googleapis.com/youtube/v3/playlists", {
-      part: 'snippet',
-      maxResults:10,
-      playlistId:pid,
-      
-      key: 'AIzaSyBs-BPSMqxLUwSi9UJ27ltcNMRTxMEMOyg' },
-    function(data) {
-      $.each(data.items, function(i, item) {
-        var vid_title=item.snippet.title;
-        pid=item.contentDetails.relatedPlaylistis.uploads;
-        fetchVideo(paid);
-        console.log(item);
-      });
-    }
-  );
-}
-});
-
-
-
-   function bSpot() {
-    $.ajax({
-      url: 'https://www.googleapis.com/youtube/v3/playlists',
-      dataType: 'json',
-      success: function(data) {
-        console.log(data);
-      }
-    });
-      }  */
-
-      
-/*======================= AJAX
-
-
-
-var daskCon = document.querySelector('#data')
-function spotifyAuh() {
-  fetch('https://accounts.spotify.com/authorize?client_id=8402bf679f8b4e6c975686305c2bc15f&scopes=user-library-read&response_type=code&&redirect_uri=https%3A%2F%2Fjansgreen.github.io%2FSoyFlow%2Fdaskboard', 37i9dQZEVXbKAbrMR8uuf7)
-    .then(res => res.json())
-    .then(data => {
-      data
-      console.log(data)
-      daskCon.innerHTML = `${data}`;
-    })
-}
-
-xhr.getAlbums(['5U4W9E5WsYb2jUQWePT8Xm', '3KyVcddATClQKIdtaap4bV'])
-  .then(function (data) {
-    console.log('Albums information', data);
-  }, function (err) {
-    console.error(err);
-  });
-
-
-
-
-
-/*=====================
-SPOTIFY AUTORIZACE AND TOKEN
-=======================
-Client_Id 8402bf679f8b4e6c975686305c2bc15f
-Client  f9408c256dd64b00ac65e8b4bdad7549
-ClientID_Secret_Together 8402bf679f8b4e6c975686305c2bc15f:f9408c256dd64b00ac65e8b4bdad7549
-ClientID_Secret_BASE64_ENCODE ODQwMmJmNjc5ZjhiNGU2Yzk3NTY4NjMwNWMyYmMxNWY6Zjk0MDhjMjU2ZGQ2NGIwMGFjNjVlOGI0YmRhZDc1NDk=
-
-curl -H "Authorization: Basic ODQwMmJmNjc5ZjhiNGU2Yzk3NTY4NjMwNWMyYmMxNWY6Zjk0MDhjMjU2ZGQ2NGIwMGFjNjVlOGI0YmRhZDc1NDk=" 
--d grant_type=authorization_code 
--d code=AQB84jEMV1ODCPe7y643Kb7OG5hYt_N1gxoZh0ZXD5Ceo0xnWXMPn8oVilRYHSdzTBbrQUddTO1UOrM1_Bl0UKClXyNtKDnHyEXM4uIhG-NUGkwXXLaKK_q
-_UC9Nfn2xfWRU8b5sn667bb8ETKSKAAggsS1duySsw5CiUwdUjoey8aGO0jvq14zKI6o1JsZ50a6zppSVSzDa4kyk-dbJs80YpA -d redirect_uri=https%3A%2F%2Fjansgreen.github.io%2FSoyFlow%2Fdaskboard https://accounts.spotify.com/api/token
-
-{"access_token":"BQAX4QnLuSdR9D1WVtKSrYBcbixBmNY5lB4619AXCkmiH5YHa1iCAFTOJjFcRgAPxtyINsVQsZLbt5DaQYrzYKOJwwfnA1bDH46Cn8RVBjOkM6HYzbZfSUAOB3C6B3DWJWoldhnFkhmbmsVvTrscC56aNvqXiEuD6w",
-"token_type":"Bearer","expires_in":3600,"refresh_token":"AQChtWqIAZEHYxciiOJ-oNPXha0SAtk2wImFN3bRikBl12ZyUmDJfDI4F5UGrtif2vivuZIdOAotHWpfdgmNshWoiZNMhu3BQ_ZvJoAK4bPQkZLcN-foNhoWikgJDEdSDNI","scope":""}
-
-====================== SPOTIFY URL
-https://accounts.spotify.com/authorize?client_id=8402bf679f8b4e6c975686305c2bc15f&scopes=user-library-read&response_type=code&&redirect_uri=https%3A%2F%2Fjansgreen.github.io%2FSoyFlow%2Fdaskboard
-=======================*/
-
-/*
-var request = require("request");
-var user_id = "lyo60r7xrt5cexv4nm7uw4wd2";
-var token = "Bearer BQAX4QnLuSdR9D1WVtKSrYBcbixBmNY5lB4619AXCkmiH5YHa1iCAFTOJjFcRgAPxtyINsVQsZLbt5DaQYrzYKOJwwfnA1bDH46Cn8RVBjOkM6HYzbZfSUAOB3C6B3DWJWoldhnFkhmbmsVvTrscC56aNvqXiEuD6w";
-var playlist_url="https://api.spotify.com/v1/playlists/"+user_id+"/playlists";
-
-  function fPlayList() {
-      fetch({url:playlist_url, headers:{Authorization:token}})
-      .then(res => res.json())
-      .then(data => {
-          console.log(data)
-
-      })
-  }*/
-
-/*================================= VAR REQUEST = REQUIRE("NO ESTA FUNCIONANDO")
-var request = require("request");
-var user_id = "lyo60r7xrt5cexv4nm7uw4wd2";
-var token = "Bearer BQAX4QnLuSdR9D1WVtKSrYBcbixBmNY5lB4619AXCkmiH5YHa1iCAFTOJjFcRgAPxtyINsVQsZLbt5DaQYrzYKOJwwfnA1bDH46Cn8RVBjOkM6HYzbZfSUAOB3C6B3DWJWoldhnFkhmbmsVvTrscC56aNvqXiEuD6w";
-var playlist_url="https://api.spotify.com/v1/playlists/"+user_id+"/playlists";
-
-request({url:playlist_url, headers:{Authorization:token}}, function(err, res) {
-  if(res){
-    var playlists=Json.parse(res.body);
-    var playlist_url = playlists.items[0].href
-    request({url:playlist_url, headers:{"Authorization":token}}, function(err, res) {
-    if (res){
-      var playlist = JSON.parse(res.body);
-      console.log("playlist:"+playlist.name);
-      playlist.tracks.items.forEach(function(track) {
-        document.getElementsById("data").innerHTML = `"playlist:"+${playlist.name}`
-        console.log(track.track.name);
-
-      });
-     }
-  })
-}
-})
-
-
-*/
-
-/*
-
-
-
-
-
-/*
-
-var OAUTH2_CLIENT_ID = '522228945921-6q3pk6hsaajtphi8pj466k4sgchds5c9.apps.googleusercontent.com';
-var OAUTH2_SCOPES = [
-  'https://www.googleapis.com/auth/youtube'
-];
-
-// Upon loading, the Google APIs JS client automatically invokes this callback.
-googleApiClientReady = function() {
-  gapi.auth.init(function() {
-    window.setTimeout(checkAuth, 1);
   });
 }
-
-// Attempt the immediate OAuth 2.0 client flow as soon as the page loads.
-// If the currently logged-in Google Account has previously authorized
-// the client specified as the OAUTH2_CLIENT_ID, then the authorization
-// succeeds with no user intervention. Otherwise, it fails and the
-// user interface that prompts for authorization needs to display.
-function checkAuth() {
-  gapi.auth.authorize({
-    client_id: OAUTH2_CLIENT_ID,
-    scope: OAUTH2_SCOPES,
-    immediate: true
-  }, handleAuthResult);
-}
-
-// Handle the result of a gapi.auth.authorize() call.
-function handleAuthResult(authResult) {
-  if (authResult && !authResult.error) {
-    // Authorization was successful. Hide authorization prompts and show
-    // content that should be visible after authorization succeeds.
-    $('.pre-auth').hide();
-    $('.post-auth').show();
-    loadAPIClientInterfaces();
-  } else {
-    // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
-    // client flow. The current function is called when that flow completes.
-    $('#login-link').click(function() {
-      gapi.auth.authorize({
-        client_id: OAUTH2_CLIENT_ID,
-        scope: OAUTH2_SCOPES,
-        immediate: false
-        }, handleAuthResult);
-    });
-  }
-}
-
-// Load the client interfaces for the YouTube Analytics and Data APIs, which
-// are required to use the Google APIs JS client. More info is available at
-// https://developers.google.com/api-client-library/javascript/dev/dev_jscript#loading-the-client-library-and-the-api
-function loadAPIClientInterfaces() {
-  gapi.client.load('youtube', 'v3', function() {
-    handleAPILoaded();
-  });
-}
-
-/*
-
-var HTMLRestSend = document.querySelector("data");
-function writeToDocument() {
-  fetch(
-    "https://apis.google.com/js/api.js"
-  )
-    .then(res => res.json())
-    .then(data => {
-      var acceAlbum =
-        data.album[`name`] +
-        "     " +
-        data.album[`artist`] +
-        "     " +
-        data.album[`image`][`0`].size +
-        "    " +
-        data.album[`url`] +
-        "     " +
-        data.album[`listeners`] +
-        "     " +
-        data.album[`playcount`] +
-        "     " +
-        data.album[`tracks`].track +
-        "     " +
-        data.album[`tags`].tag;
-
-      var x = document.createElement("TABLE");
-      x.setAttribute("id", "myTable");
-      document.body.appendChild(x);
-
-      var y = document.createElement("TR");
-      y.setAttribute("id", "myTr");
-      document.getElementById("myTable").appendChild(y);
-
-      var z = document.createElement("TD");
-      var name = document.createTextNode(`${acceAlbum}`);
-      console.log(data.album);
-
-      z.appendChild(name);
-      document.getElementById("data").appendChild(z);
-    });
-}
-*/
-/*===========================*/
-/*https://www.youtube.com/watch?v=SPIbNWqyulk
-AIzaSyB3zD_WxamQjZdhXJ08fxVpfdkukZlyLCE*/
-/*===========================*/
-
-/*
-
-const baseURL = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=9c69e58809d74e2d7b97cb3684ffdbf2&artist=all&album=all&format=json";
-
-function getData(type, cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
-}
-
-function getTableHeaders(obj) {
-    var tableHeaders = [];
-
-    Object.keys(obj).forEach(function(key) {
-        tableHeaders.push(`<td>${key}</td>`)
-    });
-
-    return `<tr>${tableHeaders}</tr>`;
-}
-
-function writeToDocument(type) {
-    var tableRows = [];
-    var el = document.getElementById("data");
-
-    getData(type, function(data) {
-        data = data.results;
-        console.log(data[`album`].name);
-
-
-        var tableHeaders = getTableHeaders(data[0]);
-
-        data.forEach(function(item) {
-            var dataRow = [];
-            Object.keys(item).forEach(function(key) {
-                var rowData = item[key].toString();
-                var truncatedData = rowData.substring(0, 15);
-                dataRow.push(`<td>${truncatedData}</td>`);
-            });
-            tableRows.push(`<tr>${dataRow}</tr>`)
-        });
-
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
-    });
-}*/
-
-/*
-
-  var artist = document.createTextNode(`${acceAlbum[`artist`]}`);
-  var url = document.createTextNode(`${acceAlbum[`url`]}`);
-  var listeners = document.createTextNode(`${acceAlbum[`listeners`]}`);
-  var playcount = document.createTextNode(`${acceAlbum[`playcount`]}`);
-  var trackst = document.createTextNode(`${acceAlbum[`tracks`]}`);
-  var tags = document.createTextNode(`${acceAlbum[`tags`]}`);
-  var fullInfo = `${name + artist + url + listeners + playcount + trackst + tags + fullInfo}`;
-
-
-===============================
-  .getJSON('your-api-url/validate.php?'+$(this).serialize+'callback=?', function(data){
-  if(data)console.log(data);
-  });
-
-   spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
-    if (err) console.error(err);
-    else console.log('Artist albums', data);
-  });
-
-  // get Elvis' albums, using Promises through Promise, Q or when
-  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-    .then(function(data) {
-      console.log('Artist albums', data);
-    }, function(err) {
-      console.error(err);
-    });
-
-    var spotifyApi = new SpotifyWebApi('https://api.spotify.com/v1/playlists/');
-spotifyApi.setAccessToken('BQAX4QnLuSdR9D1WVtKSrYBcbixBmNY5lB4619AXCkmiH5YHa1iCAFTOJjFcRgAPxtyINsVQsZLbt5DaQYrzYKOJwwfnA1bDH46Cn8RVBjOkM6HYzbZfSUAOB3C6B3DWJWoldhnFkhmbmsVvTrscC56aNvqXiEuD6w');
-spotifyApi.setPromiseImplementation(Q);
-spotifyApi.getAuthInstance('8402bf679f8b4e6c975686305c2bc15f');
-spotifyApi.setApiKey('8402bf679f8b4e6c975686305c2bc15f')
-
-spotifyApi.getArtistAlbums('17HsiXfqKUPoTP6Y5ebs1L:playlist:37i9dQZEVXbKAbrMR8uuf7')
-    .then(function(data) {
-    console.log('Artist albums', data);
- }, function(err) {
-    console.error(err);
-  });
-
-===============================================================*/
-
-
