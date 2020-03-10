fb552d41566e5c3303c24825db31e7240a562407
Luis Pena <Luis Pena>
2020-03-10 12:51:41 -0400

    I test the Auht of google fixing 42

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -160,7 +160,7 @@ const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
-var LoginBut = document.getElementsById('LoginBut');
+var LoginBut = document.getElementsById('#LoginBut');
 var LogOutBut = document.getElementsById('LogOutBut');
 var UserNameBar = document.getElementsById('UserNameBar');
 
@@ -191,28 +191,34 @@ var UserNameBar = document.getElementsById('UserNameBar');
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
         // Handle initial sign in state
         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        LoginBut.onclick = handleAuthClick;
-        LogOutBut.onclick = handleSignoutClick;
-        UserNameBar.innerHTML = gapi.currentUser.listen(listener);
+        //LoginBut.onclick = handleAuthClick;
+        //LogOutBut.onclick = handleSignoutClick;
+        //UserNameBar.innerHTML = gapi.currentUser.listen(listener);
       });
   }
+  function updateSigninStatus(isSignedIn) {
+       if (isSignedIn) {
+        $( document ).ready(function() {
 
-  $( document ).ready(function() {
-    $('#LoginBut').hide();
-    $('#content').hide();
-    $( "#LoginBut" ).click(function() {
-        $('#LogOutBut').show();
-        $('#content').show(); 
-        $('#LoginBut').hide(); 
-    });
-
-    $( "#LogOutBut" ).click(function() {
-        $('#LoginBut').show(); 
-        $('#LogOutBut').hide();
-        $('#content').hide();
-         
-    });
-});
+          $( "#LoginBut" ).click(function() {
+              $('#LogOutBut').show();
+              $('#content').show(); 
+              $('#LoginBut').hide(); 
+          });
+      
+          $( "#LogOutBut" ).click(function() {
+              $('#LoginBut').show(); 
+              $('#LogOutBut').hide();
+              $('#content').hide();
+               
+          });
+      });
+      UserNameBar.innerHTML = gapi.currentUser.listen(listener);
+    } else {
+      UserNameBar.innerHTML = gapi.currentUser.listen("Some error");
+    }
+  }
+  
 
   gapi.load("client:auth2", function() {
     gapi.auth2.init({client_id: CLIENT_ID});
