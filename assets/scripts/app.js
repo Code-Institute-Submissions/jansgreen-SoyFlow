403d4bba95187338604e209c86aac9cb34105153
Luis Pena <Luis Pena>
2020-03-10 13:52:10 -0400

    I test the Auht of google fixing 43 ====WORKING====

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -160,7 +160,11 @@ const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
-
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
+const defaultChannel = 'https://www.youtube.com/watch?v=QFs3PIZb3js&list=PLcUqPeI0P9OzpYK1UBbeAApBHkEXONi5K';
 
   function authenticate() {
     return gapi.auth2.getAuthInstance()
@@ -196,20 +200,112 @@ const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
     $( "#LoginBut" ).click(function() {
         $('#LogOutBut').show(); 
         $('#LoginBut').hide(); 
-        setTimeout( function() { window.location.href = "daskboard"; }, isSignedIn );
+        setTimeout( function() { window.location.href = "daskboard.html"; }, isSignedIn );
+        $('#content').show();
     });
 
     $( "#LogOutBut" ).click(function() {
         $('#LoginBut').show(); 
         $('#LogOutBut').hide(); 
+        $('#content').hide();
+        setTimeout( function() { window.location.href = "index.html"; }, 500 );
     });
 });
 
-   
-
-  
   gapi.load("client:auth2", function() {
     gapi.auth2.init({client_id: CLIENT_ID});
   });
+
+  function handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+  
+  // Handle logout
+  function handleSignoutClick() {
+    gapi.auth2.getAuthInstance().signOut();
+  }
+  
+  // Display channel data
+  function showChannelData(data) {
+    const channelData = document.getElementById('channel-data');
+    channelData.innerHTML = data;
+  }
+  
+  // Get channel from API
+  function getChannel(channel) {
+    gapi.client.youtube.channels
+      .list({
+        part: 'snippet,contentDetails,statistics',
+        forUsername: channel
+      })
+      .then(response => {
+        console.log(response);
+        const channel = response.result.items[0];
+  
+        const output = `
+          <ul class="collection">
+            <li class="collection-item">Title: ${channel.snippet.title}</li>
+            <li class="collection-item">Subscribers: ${numberWithCommas(
+              channel.statistics.subscriberCount
+            )}</li>
+            <li class="collection-item">Views: ${numberWithCommas(
+              channel.statistics.viewCount
+            )}</li>
+            <li class="collection-item">Videos: ${numberWithCommas(
+              channel.statistics.videoCount
+            )}</li>
+          </ul>
+          <p>${channel.snippet.description}</p>
+          <hr>
+          <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+            channel.snippet.customUrl
+          }">Visit Channel</a>
+        `;
+        showChannelData(output);
+  
+        const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+        requestVideoPlaylist(playlistId);
+      })
+      .catch(err => alert('No Channel By That Name'));
+  }
+  
+  // Add commas to number
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+  
+  function requestVideoPlaylist(playlistId) {
+    const requestOptions = {
+      playlistId: playlistId,
+      part: 'snippet',
+      maxResults: 10
+    };
+  
+    const request = gapi.client.youtube.playlistItems.list(requestOptions);
+  
+    request.execute(response => {
+      console.log(response);
+      const playListItems = response.result.items;
+      if (playListItems) {
+        let output = '<br><h4 class="center-align">Latest Videos</h4>';
+  
+        // Loop through videos and append output
+        playListItems.forEach(item => {
+          const videoId = item.snippet.resourceId.videoId;
+  
+          output += `
+            <div class="col-6">
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+          `;
+        });
+  
+        // Output videos
+        videoContainer.innerHTML = output;
+      } else {
+        videoContainer.innerHTML = 'No Uploaded Videos';
+      }
+    });
+  }
 
 
