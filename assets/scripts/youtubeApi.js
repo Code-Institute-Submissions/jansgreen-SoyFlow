b377a252a288e41a8f721a52484eb466e60ced73
Luis Pena <Luis Pena>
2020-03-10 07:52:12 -0400

    I test the Auht of google fixing 25

--- assets/scripts/youtubeApi.js
+++ assets/scripts/youtubeApi.js
@@ -3,22 +3,22 @@ API OF YOUTUBE
 ===============================================================*/
 
 // Options
-
-// Options
-const CLIENT_ID = '522228945921-6q3pk6hsaajtphi8pj466k4sgchds5c9.apps.googleusercontent.com';
-const DISCOVERY_DOCS = [
+var apiKay = 'AIzaSyBs-BPSMqxLUwSi9UJ27ltcNMRTxMEMOyg';
+var CLIENT_ID = '522228945921-6q3pk6hsaajtphi8pj466k4sgchds5c9.apps.googleusercontent.com';
+var CLIENT_ID = 'YOUR_CLIENT_ID';
+var DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
-const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+var SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
-const authorizeButton = document.getElementById('authorize-button');
-const signoutButton = document.getElementById('signout-button');
-const content = document.getElementById('content');
-const channelForm = document.getElementById('channel-form');
-const channelInput = document.getElementById('channel-input');
-const videoContainer = document.getElementById('video-container');
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
+var content = document.getElementById('content');
+var channelForm = document.getElementById('channel-form');
+var channelInput = document.getElementById('channel-input');
+var videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'techguyweb';
+var defaultChannel = 'techguyweb';
 
 // Form submit and change channel
 channelForm.addEventListener('submit', e => {
