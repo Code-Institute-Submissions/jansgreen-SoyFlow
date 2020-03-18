c37061d78b190593d0e2e1c91fe6d95d95801411
Luis Pena <Luis Pena>
2020-03-18 13:48:09 -0400

    Contract Form fixed

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -1,168 +1,104 @@
+
 /*==============================================================
-EMAILING
+CONTRACT FIELDS
 ===============================================================*/
-$(document).ready(function() {
-    $("#RomeoSantos").click(function() {
-    $("#ArtName").text("Romeo Santos");
-    $(window.location.href = "contract.html");
+$(document).ready(function(){
+  $("#conditions").click(function(){
+    var UsersName = $("#UserName").val();
+    var fullName = $("#full_name").val();
+    var Address = $("#Address").val();
+    var EvenAddress = $("#E_Address").val();
+    var usersEmail = $("#Email").val();
+    var Peaplo = $("#Peoble").val();
+    var Numberphone = $("#phone").val();
+    var Note = $("#note").val();
+    var ArtsSelect = $("#ArtSelect").val();
+
+    if (UsersName == "") {
+      $("#artfieldUserName").fadeIn();
+      $("#artfieldUserName").html("Your Companny Name is empy!!");
+      return false;
+    } else if (fullName == "") {
+      $("#artfieldFullName").fadeIn();
+      $("#artfieldFullName").html("Your Full Name is empy!!");
+      return false;
+    } else if (Address == "") {
+      $("#artfieldAddress").fadeIn();
+      $("#artfieldAddress").html("Your address is empy!!");
+      return false;
+    } else if (EvenAddress == "") {
+      $("#artfieldEvenAddress").fadeIn();
+      $("#artfieldEvenAddress").html("The Event address is empy!!");
+      return false;
+    } else if (usersEmail == "") {
+      $("#userEmail").fadeIn();
+      $("#userEmail").html("Your Email is empy!!");
+      return false;
+    }  else if (Peaplo == "") {
+      $("#Peaplo").fadeIn();
+      $("#Peaplo").html("how many people go to the event?");
+      return false;
+    }  else if (Numberphone == "") {
+      $("#Nphone").fadeIn();
+      $("#Nphone").html("your number phone is empty!!");
+      return false;
+    } else if (Note == "") {
+      $("#ContNote").fadeIn();
+      $("#ContNote").html("your number phone is empty!!");
+      return false;
+    }  else if (ArtsSelect == "") {
+      $("#Artselect").fadeIn();
+      $("#Artselect").html("your need select the Artist!!");
+      return false;
+    } else {
+      $("#ContSendding").fadeIn();
+      $("#allfields").fadeIn();
+      $("#Artselect").hide();
+      $("#ContNote").hide();
+      $("#Nphone").hide();
+      $("#Peaplo").hide();
+      $("#userEmail").hide();
+      $("#artfieldEvenAddress").hide();
+      $("#artfieldAddress").hide();
+      $("#artfieldFullName").hide();
+      $("#artfieldUserName").hide();
+      $("#allfields").html("Look great!!!");
+      return false;
+    }
+   
   });
 });
 
-var ContractEmail = $("form#ContractEmail");
-ContractEmail.submit(function(event) {
-  event.preventDefault();
+
+/*==============================================================
+EMAILING
+===============================================================*/
+var myform = $("form#ContractEmail");
+myform.submit(function(event){
+	event.preventDefault();
 
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
   var template_id = "soyflow";
 
-  ContractEmail.find("button").text("Sending...");
-  emailjs
-    .sendForm(service_id, template_id, ContractEmail[0])
-    .then(
-      function() {
-        alert("Sent!");
-        ContractEmail.find("button").text("Send");
-      },
-      function(err) {
-        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-        ContractEmail.find("button").text("Send");
-      }
-    )
-    .then(function() {
+  myform.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("please confirm that you want to send the form");
+       myform.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    }).then(function() {
       window.location.href = "https://jansgreen.github.io/SoyFlow/artist.html";
     });
   return false;
 });
 
-/*
-$('#contractForm').on('submit', function(event) {
-  event.preventDefault(); // prevent reload
-  
-  var formData = new FormData(this);
-  formData.append('service_id', 'gmail');
-  formData.append('template_id', 'soyflow');
-  formData.append('user_id', 'user_Oqsg60rcrUd7Iqtg8SvvS');
-  
-  $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
-      type: 'POST',
-      data: formData,
-      contentType: false, // auto-detection
-      processData: false // no need to parse formData to string
-  }).done(function() {
-      alert('Your mail is sent!');
-  }).fail(function(error) {
-      alert('Oops... ' + JSON.stringify(error));
-  });
-});
-*/
-/*
-var data = {
-  service_id: 'gmail',
-  template_id: 'soyflow',
-  user_id: 'user_Oqsg60rcrUd7Iqtg8SvvS',
-  template_params: {
-      'username': 'James',
-      'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
-  }
-};
-
-function Contract(contactForm) {
-var templateParams = {
-  contactFormname: 'James',
-    notes: 'Check this out!'
-  };
-  
-  emailjs.send('default_service', 'soyflow', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-  
-  $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-    type: 'POST',
-    data: JSON.stringify(data),
-    contentType: 'application/json'
-  }).done(function() {
-    alert('Your mail is sent!');
-  }).fail(function(error) {
-    alert('Oops... ' + JSON.stringify(error));
-  });
-}
-    var iUsername = $("#iUsername");
-    var iFullName = $("#iFullName");
-    var iEmail = $("#iEmail");
-    var basicUrl = $("#basicUrl");
-    var iAddress = $("#iAddress");
-    var iState = $("#iState");
-    var iCity = $("#iCity");
-    var iZip = $("#iZip");
-
-    $(document).ready(function() {
-      $("#singSendding").click(function() {
-        if (iUsername == "") {
-          $("#alrReg").html("User is empy!!!");
-          $("#alrReg").show();
-          return false;
-        } else if (iFullName == "") {
-          $("#alrReg").html("Full Name is empy!!!");
-          $("#alrReg").show();
-          return false;
-        } else if (iEmail == "") {
-          $("#alrReg").html("Email is empy!!!");
-          $("#alrReg").show();
-          return false;
-        } else if (iAddress == "") {
-          $("#alrReg").html("Address is empy!!!");
-          $("#alrReg").show();
-          return false;
-        } else if (iState == "") {
-          $("#alrReg").html("States is empy!!!");
-          $("#alrReg").show();
-          return false;
-        } else if (iCity == "") {
-          $("#alrReg").html("City is empy!!!");
-          $("#alrReg").show();
-          return false;
-        } else if (iZip == "") {
-          $("#alrReg").html("Zip Code is empy!!!");
-          $("#alrReg").show();
-          return false;
-        } else {
-          $("#alrReg").html("It look great!");
-          $("#alrReg").fadeOut(1500);
-           }
-        });
-    });
-/*
-
-    function Contract(contractForm) {
-      emailjs.send("efault_service", "soyflow",{
-        "User_name": contractForm.name.value,
-        "full_name": contractForm.full_name.value,
-        "Address": contractForm.Address.value,
-        "E_Address": contractForm.E_Address.value,
-        "Email": contractForm.Email.value,
-        "People": contractForm.Peaplo.value,
-        "phone": contractForm.phone.value,
-        "note": contractForm.note.value,
-        "project_request": contractForm.note.value
-      })
-      .then(function (response) {
-        console.log("Success Send", response);
-        
-      },
-      function (error) {
-        console.log("FAILED", error);
-        
-      })
-    }*/
-
 /*==============================================================
 VALIDATE LOGGIN
 ===============================================================*/
-/*$(document).ready(function() {
+$(document).ready(function() {
   $("#logSending").click(function() {
     var user = $("#userLog").val();
     var password = $("#passwordLog").val();
@@ -180,57 +116,6 @@ VALIDATE LOGGIN
       $("#alrUser").fadeOut(1500);
     }
   });
-});*/
+});
 
-/*==============================================================
-VALIDATE CONTRACT ART
-===============================================================*/
-/*$(document).ready(function() {
-  $("#ContSendding").click(function() {
-    var User_name = $("#User_name");
-    var full_name = $("#full_name");
-    var Address = $("#Address");
-    var E_Address = $("#E_Address");
-    var Email = $("#Email");
-    var Password = $("#Password");
-    var phone = $("#phone");
 
-    if (User_name == "") {
-      $("#alrContrUname").html("User is empy!!!");
-      $("#alrContrUname").show();
-      return false;
-    } else if (full_name == "") {
-      $("#alrContrFname").html("Full Name is empy!!!");
-      $("#alrContrFname").show();
-      return false;
-    } else if (Address == "") {
-      $("#alrContrAddress").html("Email is empy!!!");
-      $("#alrContrAddress").show();
-      return false;
-    } else if (E_Address == "") {
-      $("#alrContrEaddress").html("Address is empy!!!");
-      $("#alrContrEaddress").show();
-      return false;
-    } else if (Email == "") {
-      $("#alrContrEmail").html("Email is empy!!!");
-      $("#alrContrEmail").show();
-      return false;
-    } else if (Password == "") {
-      $("#alrContrPassword").html("Password is empy!!!");
-      $("#alrContrPassword").show();
-      return false;
-    } else if (phone == "") {
-      $("#alrContrPhone").html("Phone is empy!!!");
-      $("#alrContrPhone").show();
-      return false;
-    } else {
-      $("#alrContr").html("It look great!");
-      $("#alrContr").fadeOut(1500);
-      $(document).ready(function() {
-        setTimeout(function() {
-          window.location.href = "daskboard.html";
-        }, 500);
-      });
-    }
-  });
-});*/
